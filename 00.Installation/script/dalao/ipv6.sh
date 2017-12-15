59e3899043d43c17a62abfcbb5cca8dcc5820a36
Aniverse <369842286@qq.com>
2017-12-16 02:55:29 +0800

    new function; improve user interaction
    01. Colorize user interaction
    02. Fix default interface detection (add exception for eth1, he-ipv6, docker)
    03. Check if ifup succeeded
    04. Remove existing config, so if there is anything wrong you could re-run this script

--- 00.Installation/script/dalao/ipv6.sh
+++ 00.Installation/script/dalao/ipv6.sh
@@ -2,14 +2,19 @@
 # ipv6-dhclient-script - https://github.com/jxzy199306/ipv6-dhclient-script/
 # Modified by Aniverse
 
+black=$(tput setaf 0); red=$(tput setaf 1); green=$(tput setaf 2); yellow=$(tput setaf 3);blue=$(tput setaf 4); magenta=$(tput setaf 5); cyan=$(tput setaf 6); white=$(tput setaf 7);on_red=$(tput setab 1); on_green=$(tput setab 2); on_yellow=$(tput setab 3); on_blue=$(tput setab 4);on_magenta=$(tput setab 5); on_cyan=$(tput setab 6); on_white=$(tput setab 7); bold=$(tput bold);dim=$(tput dim); underline=$(tput smul); reset_underline=$(tput rmul); standout=$(tput smso);reset_standout=$(tput rmso); normal=$(tput sgr0); alert=${white}${on_red}; title=${standout};baihuangse=${white}${on_yellow}; bailanse=${white}${on_blue}; bailvse=${white}${on_green};baiqingse=${white}${on_cyan}; baihongse=${white}${on_red}; baizise=${white}${on_magenta};heibaise=${black}${on_white};shanshuo=$(tput blink); wuguangbiao=$(tput civis); guangbiao=$(tput cnorm)
+
 INTERFACE=$1
 BLOCK_ADDR=$2
 BLOCK_SUBNET=$3
 BLOCK_DUID=$4
-wangka=`cat /proc/net/dev |awk -F: 'function trim(str){sub(/^[ \t]*/,"",str); sub(/[ \t]*$/,"",str); return str } NR>2 {print trim($1)}'  |grep -Ev '^lo|^sit|^stf|^gif|^dummy|^vmnet|^vir|^gre|^ipip|^ppp|^bond|^tun|^tap|^ip6gre|^ip6tnl|^teql|^venet' |awk 'NR==1 {print $0}'`
+DHCLIENT6_FILE="/etc/dhcp/dhclient6.conf"
+
+[ -n "$(grep 'eth0:' /proc/net/dev)" ] && wangka=eth0 || wangka=`cat /proc/net/dev |awk -F: 'function trim(str){sub(/^[ \t]*/,"",str); sub(/[ \t]*$/,"",str); return str } NR>2 {print trim($1)}'  |grep -Ev '^lo|^sit|^stf|^gif|^dummy|^vmnet|^vir|^gre|^ipip|^ppp|^bond|^tun|^tap|^ip6gre|^ip6tnl|^teql|^venet|^docker.*|^he-ipv6' |awk 'NR==1 {print $0}'`
+
 
 if [[ "$(id -u)" != 0 ]]; then
-        echo "Sorry, you need to run this as root"
+        echo "${bold}${baihongse}ERROR${normal} ${bold}You need to run this script as root${normal}"
         exit 1
 fi
 
@@ -20,7 +25,7 @@ elif [[ -f /etc/redhat-release ]]; then
     DISTRO="Redhat"
     DEFAULT_INTERFACE="enp2s0"
 else
-    echo "This distribution is not supported"
+    echo "${bold}${baihongse}ERROR${normal} ${bold}This distribution is not supported${normal}"
     exit 1
 fi
 
@@ -28,52 +33,65 @@ while :
 do
 clear
     if ! [[ -f /proc/net/if_inet6 ]]; then
-        echo "Seems that IPv6 is not supported by your kernel or the module is not loaded (is it blacklisted?)"
+        echo "${bold}${baihongse}ERROR${normal} ${bold}Seems that IPv6 is not supported by your kernel or the module is not loaded (is it blacklisted?)${normal}"
         exit 1
     fi
 
-    echo "WARNING: Network will restart at the end of this script so any existing connections will be dropped!"
+    [[ -a $DHCLIENT6_FILE ]] && echo "${bold}${red}Attention${white} dhclient6.conf detected. Existing config will be replaced"
+    echo "${bold}${red}WARNING${white} Network will restart at the end of this script so any existing connections will be dropped!${normal}"
     echo
     
     while [[ $INTERFACE = "" ]]; do
-        read -e -p "Interface where IPv6 will be enabled: " -i $wangka INTERFACE
+        read -e -p "${bold}${yellow}Interface where IPv6 will be enabled: ${blue}" -i $wangka INTERFACE
+        echo -ne "${normal}"
     done
+
+    echo
 
     CURRENT_IPV6=$(ip addr show dev $INTERFACE | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d')
     if [[ $? -eq 0 ]]; then
-        echo "You have the following IPv6 addreses configured for $INTERFACE:"
-        echo "$CURRENT_IPV6"
-        read -e -p "Continue? [Y/n]: " -i "Y" SKIP
+        echo "${bold}You have the following IPv6 addreses configured for ${blue}$INTERFACE${white}"
+        echo "${green}${CURRENT_IPV6}${white}"
+        read -e -p "Continue? [Y/n]: ${normal}" -i "Y" SKIP
         if ! [[ $SKIP =~ ^([yY][eE][sS]|[yY])$ ]]; then
             exit 1
         fi
     fi
-    
+
+    echo
+
     while [[ $BLOCK_ADDR = "" ]]; do # to be replaced with regex
-        read -p "Your IPv6 block address (e.g. 2001:bb8:3e23:200::): " BLOCK_ADDR
+        read -p "${bold}${yellow}Input your IPv6 block address ${white}(e.g. ${underline}2001:bb8:3e23:200::${reset_underline}): ${blue}" BLOCK_ADDR
     done
     
     while ! [[ $BLOCK_SUBNET =~ ^[0-9]+$ ]]; do
-        read -p "Subnet for your block (e.g. if it's /56, input 56): " BLOCK_SUBNET
+        read -p "${bold}${yellow}Input your subnet for block ${white}(e.g. if it's /56, input ${underline}56${reset_underline}): ${blue}" BLOCK_SUBNET
     done
 
     while [[ $BLOCK_DUID = "" ]]; do # to be replaced with regex
-        read -p "Associated DUID (e.g. 00:03:00:00:34:b0:0c:47:4a:0e): " BLOCK_DUID
+        read -p "${bold}${yellow}Input your associated DUID ${white}(e.g. ${underline}00:03:00:00:34:b0:0c:47:4a:0e${reset_underline}): ${blue}" BLOCK_DUID
+        echo -ne "${normal}"
     done
 
-    echo "Working..."
+    echo; echo "${bold}Press ${on_green}ENTER${normal} ${bold}to start${normal}" ;read answer
+
+    echo;echo "${bold}Working...${normal}";echo
 
     if [[ $DISTRO = "Debian" ]]; then
         INTERFACES_FILE="/etc/network/interfaces"
+        [[ -n `tail -n1 $INTERFACES_FILE | grep IPv6_Script` ]] && sed -i '1{:a;N;'$8'!b a};$d;N;P;D' $INTERFACES_FILE
         echo "" >> $INTERFACES_FILE
+        echo "### Added by IPv6_Script ###" >> $INTERFACES_FILE
         echo "iface $INTERFACE inet6 static" >> $INTERFACES_FILE
         echo "address $BLOCK_ADDR" >> $INTERFACES_FILE
         echo "netmask $BLOCK_SUBNET" >> $INTERFACES_FILE
         echo "accept_ra 1" >> $INTERFACES_FILE
         echo "pre-up dhclient -cf /etc/dhcp/dhclient6.conf -pf /run/dhclient6.$INTERFACE.pid -6 -P $INTERFACE" >> $INTERFACES_FILE
         echo "pre-down dhclient -x -pf /run/dhclient6.$INTERFACE.pid" >> $INTERFACES_FILE
+        echo "### IPv6_Script END ###" >> $INTERFACES_FILE
     elif [[ $DISTRO = "Redhat" ]]; then
         INTERFACES_FILE="/etc/systemd/system/ipv6-dhclient.service"
+        rm -rf $INTERFACES_FILE
         echo "[Unit]" >> $INTERFACES_FILE
         echo "Description=$INTERFACE IPv6" >> $INTERFACES_FILE
         echo "After=network.target" >> $INTERFACES_FILE
@@ -91,7 +109,7 @@ clear
         echo "WantedBy=multi-user.target" >> $INTERFACES_FILE
     fi
 
-    DHCLIENT6_FILE="/etc/dhcp/dhclient6.conf"
+    rm -rf $DHCLIENT6_FILE
     echo "interface \"$INTERFACE\" {" >> $DHCLIENT6_FILE
     echo "send dhcp6.client-id $BLOCK_DUID;" >> $DHCLIENT6_FILE
     echo "request;" >> $DHCLIENT6_FILE
@@ -100,20 +118,22 @@ clear
     if [[ $DISTRO = "Debian" ]]; then
         sysctl -w net.ipv6.conf.$INTERFACE.autoconf=0
         echo "net.ipv6.conf.$INTERFACE.autoconf=0" >> /etc/sysctl.conf
-        ifdown $INTERFACE && ifup $INTERFACE
-        ifdown $INTERFACE && ifup $INTERFACE
+        ifdown $INTERFACE && ifup $INTERFACE >> ifupresult
+        [[ -n "$(tail -n1 ifupresult | grep 'Failed')" ]] && ifdown $INTERFACE && ifup $INTERFACE >> ifupresult
+        [[ -n "$(tail -n1 ifupresult | grep 'Done')" ]] && echo "Waiting for DAD... Done"
+        rm -rf ifupresult
     elif [[ $DISTRO = "Redhat" ]]; then
         systemctl enable ipv6-dhclient
         systemctl restart ipv6-dhclient
     fi
 
-    echo "Testing IPv6 connectivity..."
+    echo; echo "${bold}${green}Testing IPv6 connectivity...${normal}"; echo
     IPV6_TEST=$(ping6 -c 5 ipv6.google.com | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
     if [[ $IPV6_TEST > 0 ]]; then
-        echo "Success!"
+        echo "${bold}${green}Success!${normal}"
         exit 0
     else
-        echo "Something went wrong :("
+        echo "${bold}${red}Something went wrong :(${normal}"
         exit 1
     fi
 done
